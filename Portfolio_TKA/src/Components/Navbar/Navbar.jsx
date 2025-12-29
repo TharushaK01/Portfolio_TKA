6e08e08263a59615355e07dc01b24e7d57ce208e
TharushaK01 <tharushak01@gmail.com>
2025-12-29 23:02:03 +0530

    nav bar mobile updated

--- Portfolio_TKA/src/Components/Navbar/Navbar.jsx
+++ Portfolio_TKA/src/Components/Navbar/Navbar.jsx
@@ -3,14 +3,30 @@ import './Navbar.css'
 import logo from '../../assets/logo.webp'
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <div className='navbar'>
         <img className='nav-logo' src={logo} alt='nav-logo'/>
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
+         {/* <!-- Hamburger Icon (hidden on desktop) --> */}
+  <div class="nav-hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+        <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+            <li onClick={closeMenu}>Home</li>
+            <li onClick={closeMenu}>About Me</li>
+            <li onClick={closeMenu}>Services</li>
+            <li onClick={closeMenu}>Portfolio</li>
+             <li className="nav-connect" onClick={closeMenu}>Connect</li>
             {/* <li>Contact</li> */}
         </ul>
         <div className="nav-connect">Connect With Me</div>
