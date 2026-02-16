c8358273229dc6eb9fee2f81975954171385769b
TharushaK01 <tharushakavinda01@gmail.com>
2026-02-16 16:28:44 +0530

    logo folio updated

--- src/Components/Page/LogoFolio/LogoFolio.jsx
+++ src/Components/Page/LogoFolio/LogoFolio.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 
 const LogoFolio = () => {
  return (
-    <div className="folio-detail">
-      <h1>Logo Folio</h1>
-      <p>More details about logo projects will go here.</p>
-      <Link to="/">Back to Home</Link>
-    </div>
+//     <div className="folio-detail">
+//       {/* <h1>Logo Folio</h1>
+//       <p>More details about logo projects will go here.</p> */}
+//       <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+//   Back to Home
+// </Link>
+//     </div>
+
+  <div className="TITLE">This Page Underconstruction</div>
+    
   );
+
 };
 
 export default LogoFolio
